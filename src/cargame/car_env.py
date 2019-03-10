14c3d9204f73ae6c451040e91353e509aa9779c0
McAron4 <algot@heimerson.se>
2019-03-10 15:58:31 +0100

    added basic car env

--- src/cargame/car_env.py
+++ src/cargame/car_env.py
@@ -1,38 +1,69 @@
 import gym
-from gym import error, space, utils
+import pygame
+from gym import error, spaces, utils
 from gym.utils import seeding
 
-class SampleEnv(gym.Env):
+class CarEnv(gym.Env):
     """
     Description of environment, rewards, actions...
     """
     metadata = {'render.modes': ['human']} # human, others???
 
     def __init__(self):
-        pass
+        pygame.init()
+        self.width = 400
+        self.height = 300
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.x = 0
+        self.y = 0
 
     def step(self, action):
         """
         Args: action
         Returns: state, reward, done, info
         """
-        pass
+        if action == 0:
+            self.y += 1
+        elif action == 1:
+            self.x += 1
+        elif action == 2:
+            self.y -= 1
+        elif action == 3:
+            self.x -= 1
+        elif action == 4:
+            pass
+
+        return (self.x, self.y), 0, False, {}
 
     def reset(self):
         """
         Returns: state
         """
-        pass
+        self.x = self.width / 2
+        self.y = self.height / 2
+
+        return (self.x, self.y)
 
     def render(self, mode='human', close=False):
         """
         Args: mode, close
         Returns: ???
         """
-        pass
+        pygame.display.flip()
+        self.screen.fill((0,0,0))
+        pygame.draw.rect(self.screen, (0, 128, 255), pygame.Rect(self.x, self.y, 30, 20))
+        
 
     def close(self):
         """
         Close window if viewer is open
         """
-        pass
+        
+
+
+
+
+
+
+
+
--- src/cargame/test.py
+++ src/cargame/test.py
@@ -4,8 +4,9 @@ import pygame
 env = CarEnv()
 
 state = env.reset()
+close_screen = False
 
-for i in range(100):
+while True:
     action = 4
 
     for event in pygame.event.get():
@@ -13,9 +14,13 @@ for i in range(100):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT: action = 1
         if event.type == pygame.KEYDOWN and event.key == pygame.K_UP: action = 2
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT: action = 3
-
+        if event.type == pygame.QUIT:
+            close_screen = True
     next_state, reward, done, info = env.step(action)
+    env.render()
 
-    if done:
+    if done or close_screen:
         break
+pygame.display.quit()
+pygame.quit()
 
